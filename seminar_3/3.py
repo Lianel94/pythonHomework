bf15ea555c40c52ce76d77a7eeb3cad579db881e
Viktoriya Krasovskaya <endi.vest@yandex.ru>
2023-04-17 15:41:28 +0500

    task two solved

--- seminar_3/3.py
+++ seminar_3/3.py
@@ -21,3 +21,27 @@ for i in arr:
 
 print(count)
 
+
+""" Задача 18: Требуется найти в массиве A[1..N] самый близкий по величине элемент к заданному числу X. Пользователь в первой строке вводит натуральное число N – количество элементов в массиве. В последующих  строках записаны N целых чисел Ai. Последняя строка содержит число X
+*Пример:*
+5
+1 2 3 4 5
+6
+-> 5 """
+
+n = int(input("Type the number of elements in the array: "))
+array = []
+for i in range(n):
+    array.append(random.randint(1, 10))
+print(array)
+
+x = int(input("Type the number: "))
+close_num = array[0]
+
+for i in array:
+    if abs(x - i) < abs(x - close_num):
+        close_num = i
+print(f"The closest number is {close_num}")
+
+
+
